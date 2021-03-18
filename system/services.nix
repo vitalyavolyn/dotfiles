47b6d650246a4eeb67656cc73eae1592f4e44be5
Vitaliy Volynskiy <i@vitalya.me>
2021-03-18 22:23:29 +0600

    Allow me to use xbacklight

--- system/services.nix
+++ system/services.nix
@@ -58,4 +58,8 @@
     enable = true;
     enableOnBoot = false;
   };
+
+  services.udev.extraRules = ''
+    ACTION=="add", SUBSYSTEM=="backlight", KERNEL=="intel_backlight", MODE="0666", RUN+="${pkgs.coreutils}/bin/chmod a+w /sys/class/backlight/%k/brightness"
+  '';
 }
