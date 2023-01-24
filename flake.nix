fcc33668dd90424065cf3cc036c383a31be3a752
Vitaliy Volynskiy <i@vitalya.me>
2023-01-24 14:00:56 +0600

    window rule tweaks

--- flake.nix
+++ flake.nix
@@ -54,9 +54,10 @@
   };
 
   nixConfig = {
-    extra-substituters = [ "https://hyprland.cachix.org" ];
+    extra-trusted-substituters = [ "https://hyprland.cachix.org" ];
     extra-trusted-public-keys = [
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
+    trusted-users = [ "root" "vitalya" ];
   };
 }
