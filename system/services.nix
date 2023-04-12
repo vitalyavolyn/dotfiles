1d11b3c20bbf0ec4a36dbe7708906851d12bfdd6
Vitaliy Volynskiy <i@vitalya.me>
2023-04-12 22:01:41 +0600

    bluetooth works somehow

--- system/services.nix
+++ system/services.nix
@@ -39,33 +39,6 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # TODO: these options are disabled now, may have broken BT audio in the process
-    #media-session.config.bluez-monitor.rules = [
-    #  {
-    #    # Matches all cards
-    #    matches = [{ "device.name" = "~bluez_card.*"; }];
-    #    actions = {
-    #      "update-props" = {
-    #        "bluez5.reconnect-profiles" = [ "hfp_hf" "hsp_hs" "a2dp_sink" ];
-    #        # mSBC is not expected to work on all headset + adapter combinations.
-    #        "bluez5.msbc-support" = true;
-    #        # SBC-XQ is not expected to work on all headset + adapter combinations.
-    #        "bluez5.sbc-xq-support" = true;
-    #      };
-    #    };
-    #  }
-    #  {
-    #    matches = [
-    #      # Matches all sources
-    #      { "node.name" = "~bluez_input.*"; }
-    #      # Matches all outputs
-    #      { "node.name" = "~bluez_output.*"; }
-    #    ];
-    #    actions = {
-    #      "node.pause-on-idle" = false;
-    #    };
-    #  }
-    #];
   };
 
   virtualisation.docker = {
