9e719916c5a5299ae15007c945ca4038d69b910c
Vitaliy Volynskiy <i@vitalya.me>
2024-05-28 15:10:41 +0500

    Change ssh key

--- modules/user.nix
+++ modules/user.nix
@@ -3,14 +3,18 @@
 {
   nix.settings.trusted-users = [ "vitalya" ];
 
-  users.users.vitalya = {
-    extraGroups = [ "wheel" "networkmanager" "docker" "video" "input" "audio" ];
-    isNormalUser = true;
-    createHome = true;
-    shell = pkgs.zsh;
-    openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC71Nu6rRV7ZHo9fS0B+Ny9bbSDVWqKU5034jwh1CZLAKzWaX3Ewj8Pb63HVN24Gt2qccKqhTEA2831UFSZX400X8PVTruItKboX81c7z4SKNUDClXkMMVt6Qw343p0viQSOdQmuX0ZPIg9x4wOO5aj5FFHBuolNlFLOFLFrl7gxpxj1H0B8/ERV1vInnJAtwr4pviruUygEmA9j5vWbIRvX1vHhJJ6fX93jBF6wMerfom4+dvicIoA17OKp/UbDcYjutx3eTWX2pXkmsPkLStIb/5RqVQ6wRdSbgVi0tZGP2WtU9kzEOITvi4rRPvgjm7gr5POyJmiVRHV/vSR5TiH vitalya@celebi"
-    ];
+  users = {
+    mutableUsers = false;
+
+    users.vitalya = {
+      extraGroups = [ "wheel" "networkmanager" "docker" "video" "input" "audio" ];
+      isNormalUser = true;
+      createHome = true;
+      shell = pkgs.zsh;
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDJOIQWALhrUwF6a23G9g3i/LjI50Bl/PGO1RauHJBks vitalya@celebi"
+      ];
+    };
   };
 
   security.sudo.wheelNeedsPassword = false;
