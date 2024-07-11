eaf3ac614f522a88ee6b658078f916a1a68f3183
Vitaliy Volynskiy <i@vitalya.me>
2024-07-11 15:38:25 +0500

    Add formatter, update stuff

--- flake.nix
+++ flake.nix
@@ -119,6 +119,9 @@
           ./hosts/applin/configuration.nix
         ];
       };
+
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+      formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixpkgs-fmt;
     };
 
   nixConfig = {
