9a574f6505466206e77ef2a3dd4a7e29385e93fd
Vitaliy Volynskiy <i@vitalya.me>
2025-08-05 19:19:18 +0500

    add dbeaver, update inputs

--- modules/apps/dev/db.nix
+++ modules/apps/dev/db.nix
@@ -5,10 +5,12 @@ with lib;
     (if (builtins.hasAttr "homebrew" options) then {
       homebrew.casks = [
         "mongodb-compass"
+        "dbeaver-community"
       ];
     } else {
       home-manager.users.vitalya.home.packages = with pkgs; [
         mongodb-compass
+        dbeaver-bin
       ];
     })
   ];
