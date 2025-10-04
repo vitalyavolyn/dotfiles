dd0997224416cfab41eb93f57717aab5230612e0
Vitaliy Volynskiy <i@vitalya.me>
2025-10-04 23:50:36 +0500

    install qbittorrent via cask

--- modules/apps/torrent.nix
+++ modules/apps/torrent.nix
@@ -1,7 +1,11 @@
-{ pkgs, ... }:
-
+{ pkgs, lib, options, ... }:
+with lib;
 {
-  home-manager.users.vitalya.home.packages = with pkgs; [
-    qbittorrent
+  config = mkMerge [
+    (if (builtins.hasAttr "homebrew" options) then {
+      homebrew.casks = [ "qbittorrent" ];
+    } else {
+      home-manager.users.vitalya.home.packages = with pkgs; [ qbittorrent ];
+    })
   ];
 }
