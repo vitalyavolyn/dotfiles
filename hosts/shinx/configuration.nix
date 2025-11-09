e251595777f927d55c33451ef95995a2fef0dd61
Vitaliy Volynskiy <i@vitalya.me>
2025-11-09 15:54:00 +0500

    remove homepage (never used it)

--- hosts/shinx/configuration.nix
+++ hosts/shinx/configuration.nix
@@ -11,20 +11,6 @@
     media-server
     { services.deluge.config.download_location = "/mnt/media/downloads/complete"; }
 
-    homepage
-    {
-      modules.homepage = {
-        baseUrl = "http://shinx.local";
-        # these services are local, so api keys are fine to be public
-        # (if you somehow get access to them, i encourage you to watch some pokemon)
-        sonarrKey = "f63ac2c47c514fb5b44d3d73adec183e";
-        radarrKey = "c703983ff599433785182afc1b91ea84";
-        bazarrKey = "f33e058ffcbe7250bfa777b9231e2125";
-        prowlarrKey = "2747ae3760784638b271cea5611c4b11";
-        jellyfinKey = "dd16672806394eaeae1e5e4000d136a3";
-      };
-    }
-
     home-assistant
     { modules.home-assistant.volumes = [ "/mnt/media/home-assistant:/config" ]; }
 
@@ -35,6 +21,7 @@
     paperless-ai
     {
       services.paperless-concierge = {
+        # TODO: why is this not in secrets?
         envFile = "/etc/paperless-concierge/.env";
       };
     }
@@ -43,7 +30,7 @@
     avahi
     # docker
     firefox
-    # spotify
+    spotify
   ];
 
   networking = {
