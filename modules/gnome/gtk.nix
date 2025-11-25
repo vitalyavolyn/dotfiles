a3a6afdfe670080754f7a2eb9b5a0bd60cd060fb
Vitaliy Volynskiy <i@vitalya.me>
2025-11-25 22:09:23 +0500

    remove old gtk theme

--- modules/gnome/gtk.nix
+++ modules/gnome/gtk.nix
@@ -13,10 +13,10 @@
 
     gtk = {
       enable = true;
-      theme = {
-        package = pkgs.arc-theme;
-        name = "Arc-Dark";
-      };
+      # theme = {
+      #   package = pkgs.arc-theme;
+      #   name = "Arc-Dark";
+      # };
       iconTheme = {
         package = pkgs.paper-icon-theme;
         name = "Paper";
