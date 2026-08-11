a05a1c69d6c3c9749c9b7d364e93f3d95b231e48
Vitaliy Volynskiy <i@vitalya.me>
2026-08-11 14:55:32 +0500

    podman tweaks

--- modules/services/podman.nix
+++ modules/services/podman.nix
@@ -4,6 +4,8 @@
   virtualisation.podman = {
     enable = true;
     autoPrune.enable = true;
+    dockerCompat = true;
+    defaultNetwork.settings.dns_enabled = true;
   };
 
   virtualisation.oci-containers.backend = "podman";
