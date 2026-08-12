a08ecc90ca73a55ee2a28d880e7beee569279e00
Vitaliy Volynskiy <i@vitalya.me>
2026-08-12 13:54:28 +0500

    fix podman dns again

--- modules/services/podman.nix
+++ modules/services/podman.nix
@@ -6,7 +6,6 @@
       enable = true;
       autoPrune.enable = true;
       dockerCompat = true;
-      defaultNetwork.settings.dns_enabled = true;
     };
 
     virtualisation.oci-containers.backend = "podman";
