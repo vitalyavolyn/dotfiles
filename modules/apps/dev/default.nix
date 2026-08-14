629f34bd26b2f9701c5b755f8087db98219b8981
Vitaliy Volynskiy <i@vitalya.me>
2026-08-15 00:33:46 +0500

    i forgor

--- modules/apps/dev/default.nix
+++ modules/apps/dev/default.nix
@@ -2,7 +2,7 @@
 
 {
   den.aspects.dev = {
-    includes = with den.aspects; [ dev-db zed claude-code ];
+    includes = with den.aspects; [ dev-db zed claude-code codex-cli ];
     homeManager = { pkgs, ... }: {
       home.packages = with pkgs; [ gnumake nixf ];
     };
